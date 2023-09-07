703bedf0e7e2a0acfbe226016d9114bc356e6940
enoqueJonas <enoque.macanda@gmail.com>
2023-09-07 14:28:03 +0200

    REFACTOR redux logic into different files

--- src/redux/reducers/repositoriesReducer.ts
+++ src/redux/reducers/repositoriesReducer.ts
@@ -1,29 +1,10 @@
+import { ActionType } from "../action-types";
+import { Action } from "../actions";
+
 interface RepositoriesState {
   loading: boolean;
   error: string | null;
   data: string[];
-}
-
-interface SerachRepositoriesAction {
-  type: ActionType.SEARCH_REPOSITORIES;
-}
-
-interface SearchRepositoriesSuccessAction {
-  type: ActionType.SEARCH_REPOSITORIES_SUCCESS;
-  payload: string[];
-}
-
-interface SearchRepositoriesErrorAction {
-  type: ActionType.SEARCH_REPOSITORIES_ERROR;
-  payload: string;
-}
-
-type Action = SerachRepositoriesAction | SearchRepositoriesSuccessAction | SearchRepositoriesErrorAction;
-
-enum ActionType {
-  SEARCH_REPOSITORIES = 'search_repositories',
-  SEARCH_REPOSITORIES_SUCCESS = 'search_repositories_success',
-  SEARCH_REPOSITORIES_ERROR = 'search_repositories_error',
 }
 
 const reducer = (state: RepositoriesState, action: Action): RepositoriesState => {

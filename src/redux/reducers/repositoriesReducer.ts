8b3f61d8a68b39f7c1172625e9b312bed50a67fa
enoqueJonas <enoque.macanda@gmail.com>
2023-09-07 13:57:27 +0200

    REFACTOR repositoriesReducer.ts to make use of TypeScript type system

--- src/redux/reducers/repositoriesReducer.ts
+++ src/redux/reducers/repositoriesReducer.ts
@@ -4,16 +4,36 @@ interface RepositoriesState {
   data: string[];
 }
 
-const reducer = (state: RepositoriesState, action: any) => {
-  const { type, payload} = action;
+interface SerachRepositoriesAction {
+  type: ActionType.SEARCH_REPOSITORIES;
+}
 
-  switch(type) {
-    case 'search_repositories':
+interface SearchRepositoriesSuccessAction {
+  type: ActionType.SEARCH_REPOSITORIES_SUCCESS;
+  payload: string[];
+}
+
+interface SearchRepositoriesErrorAction {
+  type: ActionType.SEARCH_REPOSITORIES_ERROR;
+  payload: string;
+}
+
+type Action = SerachRepositoriesAction | SearchRepositoriesSuccessAction | SearchRepositoriesErrorAction;
+
+enum ActionType {
+  SEARCH_REPOSITORIES = 'search_repositories',
+  SEARCH_REPOSITORIES_SUCCESS = 'search_repositories_success',
+  SEARCH_REPOSITORIES_ERROR = 'search_repositories_error',
+}
+
+const reducer = (state: RepositoriesState, action: Action): RepositoriesState => {
+  switch(action.type) {
+    case ActionType.SEARCH_REPOSITORIES:
       return { loading: true, error: null, data: [] }
-    case 'serach_repositories_success':
-      return { loading: false, error: null, data: payload }
-    case 'serch_repositories_error':
-      return { loading: false, error: payload, data: [] }
+    case ActionType.SEARCH_REPOSITORIES_SUCCESS:
+      return { loading: false, error: null, data: action.payload }
+    case ActionType.SEARCH_REPOSITORIES_ERROR:
+      return { loading: false, error: action.payload, data: [] }
     default:
       return state;
   }
